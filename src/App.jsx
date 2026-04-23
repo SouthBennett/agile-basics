17354e0f8828256a031314043042d12c7a49a1f3
Connor <connorjohnhughes@gmail.com>
2026-04-23 00:14:53 -0700

    Added Search bar with filtering options

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Topbar from "./components/Topbar";
@@ -7,17 +8,52 @@ import ArtistList from "./components/ArtistList";
 import Player from "./components/Player";
 import data from "./data.json";
 
+
 function App() {
   console.log(data);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState("All");
+  const [sortOption, setSortOption] = useState("name-asc");
+
+  const filteredArtists = data.artists
+      .filter((artist) => {
+      const matchedSearch = 
+      artist.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      artist.genre.toLowerCase().includes(selectedGenre.toLowerCase());
+    
+    const matchedGenre = 
+      selectedGenre === "All" || artist.genre === selectedGenre;
+    
+      return matchedSearch && matchedGenre;
+    })
+    .sort ((a,b) => {
+      if(sortOption === "name-asc"){
+        return a.name.localeCompare(b.name);
+      }
+
+      if(sortOption === "name-desc"){
+        return b.name.localeCompare(a.name);
+      }
+      return 0;
+    });
+
+
 
   return (
     <div className="app">
       <Sidebar />
       <main className="main-content">
-        <Topbar data={data} />
+        <Topbar data={data} 
+          serachTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          selectedGenre={selectedGenre}
+          setSelectedGenre={setSelectedGenre}
+          sortOption={sortOption}
+          setSortOption={setSortOption}
+        />
         <Hero />
         {/* <ArtistCard /> */}
-        <ArtistList artists={data.artists} />
+        <ArtistList artists={filteredArtists} />
       </main>
 
       <Player />
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,6 +1,13 @@
 import './Topbar.css'
 
-function Topbar({data}) {
+function Topbar({data,
+      searchTerm,
+      setSearchTerm,
+      selectedGenre,
+      setSelectedGenre,
+      sortOption,
+      setSortOption
+      }) {
   return (
     <header className="topbar">
       <div>
@@ -12,8 +19,24 @@ function Topbar({data}) {
       <input
         type="text"
         className="search-bar"
-        placeholder="Search songs, artists, albums..."
+        placeholder="Search genres, artists, albums..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
+      <select value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)}>
+      <option value="All">All Genres</option>
+      <option value="Indie Pop">Indie Pop</option>
+      <option value="Folk Rock">Folk Rock</option>
+      <option value="Synthwave">Synthwave</option>
+      <option value="Dream Pop">Dream Pop</option>
+      <option value="Post-Rock">Post-Rock</option>
+      </select>
+
+    <select value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+      <option value="name-asc">A-Z</option>
+      <option value="name-desc">Z-A</option>
+    </select>
+
       <button className="profile-btn">Profile</button>
     </header>
   )
